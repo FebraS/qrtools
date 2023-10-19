c8131133980c422aec056f53a800f7d39092f282
Febra <148491377+FebraS@users.noreply.github.com>
2023-10-20 03:35:50 +0800

    Update qrtools.py

--- qrtools.py
+++ qrtools.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-#Authors:
-#   0xFebra <0xfebra@gmail.com>
+#Authors : FebraS <0xfebra@gmail.com>
 
 import argparse
 import qrcode
@@ -9,25 +8,24 @@ import random
 
 parser = argparse.ArgumentParser(description="QR Code Generator")
 
-#create a menu using argparse
-#declare argument
+#Membuat menu menggunakan argparse
 parser.add_argument('-g','--generate',type=str,help='Generate QR Code',required=True)
 parser.add_argument('-o','--output',type=str,help='Default QR Ouput Name : \"ScanMe\"',required=False)
 args = parser.parse_args()
 
 def qr_generate(generate,output):
-    #the filename into a ascii letters
+    #Membuat nama file
     letters = string.ascii_letters
-    #random letters in 10 characters
+    #Mengacak nama file
     letters = ''.join(random.choice(letters)for i in range(10))
     
     #generating process
     qr = qrcode.make(generate)
     if output is not None:
-        #if the user inputs a file name using the -o argument
+        #Jika user menggunakan argumen -o
         qr.save("output/"+ output + ".png")
     else:
-        #if not then use random filename
+        #Gunakan nama acak
         qr.save("output/"+ letters +".png")
         print("Your QR are Your QR Code has been created with the name "+ letters +".png")
 
